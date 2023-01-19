7bc48af0ad7169c50f4f992280cb992ef0449b74
Abhishek Choudhari <abhishekc97@gmail.com>
2023-01-19 16:49:32 +0530

    react boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-    return <div className="App">Daily Notes taker app</div>;
+	return (
+		<div className="App">
+			<div className="app-container">
+				App container
+				<AppRouter />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/router/AppRouter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./AppRouter.module.css";
+
+function AppRouter() {
+    return ( 
+        <>
+            App Router
+        </>
+     );
+}
+
+export default AppRouter;
