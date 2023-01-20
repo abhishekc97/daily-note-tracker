2398fbb0dd019493043737ae2246c7238d35abd9
Abhishek Choudhari <abhishekc97@gmail.com>
2023-01-20 12:02:50 +0530

    added notes and bookmark toggle components

--- src/components/ToggleWindow/ToggleWindow.js
+++ src/components/ToggleWindow/ToggleWindow.js
@@ -2,20 +2,25 @@ import React from "react";
 import "./ToggleWindow.css";
 import { Link, useLocation } from "react-router-dom";
 
-
 function ToggleWindow() {
     return (
         <div className="toggle-window">
-            <div className="sidebar-heading">
-                DAILY NOTE TRACKER
+            <div className="sidebar-heading-box">
+                <div className="sidebar-heading-icon"></div>
+                <div>
+                    <span className="heading-title">DAILY NOTE TRACKER</span>
+                </div>
             </div>
             <div className="option-box">
-                Daily Notes
+                <div className="option-icon-1"></div>
+                <div className="option-name-1">Daily Notes</div>
             </div>
             <div className="option-box">
-                Bookmarks
+                <div className="option-icon-2"></div>
+                <div className="option-name-2">Bookmarks</div>
             </div>
-        </div> );
+        </div>
+    );
 }
 
 export default ToggleWindow;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,23 +1,31 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
 import InputBox from "../components/InputBox/InputBox";
 import NotesWindow from "../components/NotesWindow/NotesWindow";
 import ToggleWindow from "../components/ToggleWindow/ToggleWindow";
 import "./AppRouter.css";
 
 function AppRouter() {
-	return (
-		<div className="app-container">
-			<div className="left-container">
-				<ToggleWindow />
-			</div>
-			<div className="right-container">
-				Right side
-				<NotesWindow />
-				<InputBox />
-			</div>
-		</div>
-	);
+	const [currentWindow, setCurrentWindow] = useState('');
+
+	function setWindow() {
+		
+	}
+		
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <div className="left-container">
+                    <ToggleWindow />
+                </div>
+                <div className="right-container">
+                    Right side
+                    <NotesWindow />
+                    <InputBox />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default AppRouter;
